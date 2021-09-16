6fda476e9c9ad7d46d5dc704bce0e977a1d87ac0
user <user@DESKTOP-1EE4V57>
2021-09-16 17:43:33 +0900

    init project

---
+++ BSI/src/main/java/com/team4/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.team4.controller;
+
+import java.util.Locale;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	
+	 
+	@RequestMapping(path = { "/", "/home" }, method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		
+		return "home";
+	}
+	
+}
